a70a70342315684107af4c232969b51913ace8ff
suleyman <suleymanbingul35@gmail.com>
2020-06-21 11:16:26 +0100

    ikinci commit local repository den

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -5,7 +5,9 @@ public class FirstClass {
 	public static void main(String[] args) {
 		
 		System.out.println("first commit from local repository");
-
+        System.out.println("second commit from local repository");
+		
+		
 	}
 
 }
